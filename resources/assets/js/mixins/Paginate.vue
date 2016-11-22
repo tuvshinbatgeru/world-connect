5d9e29bb989fb6125b44696e8d61b573ce44472e
AkilLis <g.tuvshinbat@yahoo.com>
2016-11-23 02:28:32 +0800

    paginateable feature

---
+++ resources/assets/js/mixins/Paginate.vue
@@ -0,0 +1,114 @@
+<template>
+  <ul class="pagination"> 
+    <li v-show="pageIndex > 1">
+      <a @click="resetPage()" aria-label="First Page">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li> 
+    <li v-show="pageIndex > 1">
+      <a @click="prevPage()" aria-label="Previous">
+        <span aria-hidden="true">&lsaquo;</span>
+      </a>
+    </li> 
+
+    <li v-for="n in pages" :class="isActive(n)">
+        <a @click="toPage(n)">{{n}}</a>
+    </li>
+
+    <!-- <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>  -->
+    <li v-show="pageIndex < totalPage">
+      <a @click="nextPage()" aria-label="Next">
+        <span aria-hidden="true">&rsaquo;</span>
+      </a>
+    </li> 
+    <li v-show="pageIndex < totalPage">
+      <a @click="lastPage()" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li> 
+  </ul>
+</template>
+<script>
+  export default {
+    props : {
+        paginate : {
+          type : Number,
+          default : 5
+        },
+
+        offset: {
+          type: Number,
+          default : 3
+        },
+
+        total : {
+          type: Number,
+          required : true
+        }
+    },
+
+    data () {
+      return {
+          pageIndex : 1,
+          totalPage : 0,
+      }
+    },
+
+    created : function () {
+        this.totalPage = Math.ceil(this.total / this.paginate)
+        this.resetPage()
+    },
+
+    methods: {
+        
+        resetPage : function () {
+            this.pageIndex = 1
+            this.$emit('page-changed', this.pageIndex)
+        },
+
+        lastPage : function () {
+            this.pageIndex = this.totalPage
+            this.$emit('page-changed', this.pageIndex)
+        },
+
+        nextPage : function () {
+            this.pageIndex ++
+            this.$emit('page-changed', this.pageIndex)
+        },
+
+        prevPage : function () {
+            this.pageIndex --
+            this.$emit('page-changed', this.pageIndex)
+        },
+
+        toPage : function (page) {
+            this.pageIndex = page
+            this.$emit('page-changed', this.pageIndex)
+        },
+
+        isActive : function (index) {
+            return this.pageIndex == index ? 'active' : ''
+        }
+    }, 
+
+    computed : {
+        pages : function () {
+            var from = this.pageIndex - this.offset
+
+            if (from < 1) {
+                from = 1
+            }
+
+            var to = from + (this.offset * 2)
+
+            if (to >= this.totalPage) {
+                to = this.totalPage;
+            }            
+
+            var pages = _.range(from, to + 1)
+            return pages
+        },
+    }
+
+  }
+</script>
---
+++ resources/assets/js/mixins/Paginateable.js
@@ -0,0 +1,22 @@
+export default {
+  props : {
+    loading : {
+      type : Boolean,
+      default : false
+    }
+  },
+
+  data () {
+    return {
+        pageIndex : 1,
+        pageLast : 0,
+        total : 0,
+    }
+  },
+
+  methods: {
+      resetPage : function () {
+          this.pageIndex = 1
+      }
+  }
+}
