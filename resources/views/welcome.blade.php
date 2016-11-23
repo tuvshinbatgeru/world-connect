2a0b741e1ed2c59c7a42d6047e9b92e1a9974aab
AkilLis <g.tuvshinbat@yahoo.com>
2016-11-23 21:01:44 +0800

    album slide

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.index-layout', ['currentView' => 'index-view'])
 @section('content') 
-	<section class="content">
+	<section class="content" v-cloak>
 		<div style="width: 100%;">
 			<horizontal-slide :slide-width="100" width-type="%" :step="1" :items="items">
 	                
@@ -54,53 +54,84 @@
 		<div class="cntr-fluid">	
 			<div class="row margin-vertical">
 				<div class="col-md-9">
-					<div class="feed-container">
-						<div class="feed-title">
-							СҮҮЛД НЭМЭГДСЭН МЭДЭЭЛЭЛ
-						</div>
+					<div class="row">
+						<div class="col-md-12">
+							<div class="feed-container">
+								<div class="feed-title">
+									СҮҮЛД НЭМЭГДСЭН МЭДЭЭЛЭЛ
+								</div>
 
-						<div class="content-container" :class="$index != 0 ? 'small' : ''" v-for="news in latestNews">
-							<div class="cover-cntr">
-								<div class="cover">
-									<div class="cover-img">
-										<img class="horizontal" :src="news.cover_url"/>
-									</div>
-								</div>
-							</div>
-							<div class="content">
-								<div class="content-title">
-									@{{news.title}}
-									<label class="date">
-										@{{news.created_at | moment 'from'}}
-									</label>
-								</div>
-								<div class="content-description">
-									@{{news.info[0].description}}
-								</div>
-								<div class="content-info">
-									<div class="row">
-										<div class="col-xs-6">
-											<span class="share social-share facebook">
-												SHARE
-											</span>
-											<span class="tweet social-share twitter">
-												TWEET
-											</span>
+								<div class="content-container" :class="$index != 0 ? 'small' : ''" v-for="news in latestNews">
+									<div class="cover-cntr">
+										<div class="cover">
+											<div class="cover-img">
+												<img class="horizontal" :src="news.cover_url"/>
+											</div>
 										</div>
-										<div class="col-xs-6 text-right">
-											<span class="view">
-												<i class="fa fa-eye"></i> @{{news.visit_count}}
-											</span>
-											<span class="news-type">
-												@{{news.type | newsFilter}}
-											</span>
-										</div>	
+									</div>
+									<div class="content">
+										<div class="content-title">
+											@{{news.title}}
+											<label class="date">
+												@{{news.created_at | moment 'from'}}
+											</label>
+										</div>
+										<div class="content-description">
+											@{{news.info[0].description}}
+										</div>
+										<div class="content-info">
+											<div class="row">
+												<div class="col-xs-6">
+													<span class="share social-share facebook">
+														SHARE
+													</span>
+													<span class="tweet social-share twitter">
+														TWEET
+													</span>
+												</div>
+												<div class="col-xs-6 text-right">
+													<span class="view">
+														<i class="fa fa-eye"></i> @{{news.visit_count}}
+													</span>
+													<span class="news-type">
+														@{{news.type | newsFilter}}
+													</span>
+												</div>	
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
+						</div>
+						<div class="col-md-12">
+							<div id="container">
+							</div>
+
+							<div id="albumSlide" class="modal" data-easein="fadeIn" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
+						      <div class="modal-dialog modal-lg">
+						        <div class="modal-content">
+						          <div class="modal-header">
+						            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+						            <h4 class="modal-title" id="myModalLabel">Анхааруулга</h4>
+						          </div>
+						          <div class="modal-body">
+						          	<span>Та</span>
+						          	<strong>"@{{selectedCountry.name}}"</strong>
+						            <span>улсыг устгахдаа итгэлтэй байна уу?</span>
+						          </div>
+						          <div class="modal-footer">
+						            <button class="btn btn-danger btn-fill" data-dismiss="modal" aria-hidden="true">Үгүй</button>
+						            <button @click="deleteCountry()" data-dismiss="modal" aria-hidden="true" class="btn btn-success btn-fill">Тийм</button>
+						          </div>
+						        </div>
+						      </div>
+						    </div>
 						</div>
 					</div>
 				</div>
+
+				
+
 				<div class="col-md-3">
 					<div class="row">
 						<div class="col-md-12">
@@ -108,10 +139,17 @@
 								<div class="feed-title">
 									Валютын ханш
 								</div>
-								<div class="row" v-for="xansh in xanshs">
-									<p>@{{xansh.code}}</p>
-									<p>@{{xansh.rate}}</p>
-								</div>
+								<table class="currency-table">
+									<tr v-for="xansh in xanshs">
+										<td>
+											<i class="@{{xansh.code}}"></i>
+											@{{xansh.code}}
+										</td>
+										<td>
+											@{{xansh.rate}}
+										</td>
+									</tr>
+								</table>
 							</div>
 						</div>
 						<div class="col-md-12">
@@ -138,10 +176,34 @@
 								</div>
 							</div>
 						</div>
+
+						<div class="col-md-12">
+							<div class="feed-container mini">
+								<div class="feed-title">
+									Сошиал хуудас
+								</div>
+								<div style="padding: 28px 26px;">
+									<div class="fb-page" data-href="https://www.facebook.com/worldconnect.international" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
+
+
+
 			</div>
 		</div>
+
+		<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1549615252014368";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
 
 		<map style="height: 300px; display: block;" 
 		  :center="{lat:47.913995, lng:106.916081}"
