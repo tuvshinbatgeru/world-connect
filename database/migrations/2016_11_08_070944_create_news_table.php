a99fbefc80c6860461fc9a4188bd82373203e16f
AkilLis <g.tuvshinbat@yahoo.com>
2016-11-23 13:18:30 +0800

    pinned, view_order added

--- database/migrations/2016_11_08_070944_create_news_table.php
+++ database/migrations/2016_11_08_070944_create_news_table.php
@@ -20,6 +20,8 @@ class CreateNewsTable extends Migration
             $table->tinyInteger('type'); //1 - news, 2 - zar, 3 - tetgeleg
             $table->integer('country_id')->unsigned()->index();
             $table->integer('visit_count')->unsigned();
+            $table->tinyInteger('view_order')->default(0);
+            $table->enum('is_pinned', ['N', 'Y'])->default('N'); 
             $table->enum('is_active', ['N', 'Y'])->default('Y'); 
             $table->timestamps();
         });
