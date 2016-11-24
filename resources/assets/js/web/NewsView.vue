a22edf5758e7af014249d54f6670a15a6b1b6691
AkilLis <g.tuvshinbat@yahoo.com>
2016-11-24 22:53:55 +0800

    NewsView

---
+++ resources/assets/js/web/NewsView.vue
@@ -0,0 +1,15 @@
+<script>
+	import RelatedNews from '../components/RelatedNews.vue'
+
+	export default {
+		props : {
+			title : {
+				default : ''
+			},
+		},
+
+		components : {
+			RelatedNews
+		}
+	}
+</script>
