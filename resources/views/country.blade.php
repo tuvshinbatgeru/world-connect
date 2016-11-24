54c8eb7dce5a60405cf670fb5901bccc0b962401
AkilLis <g.tuvshinbat@yahoo.com>
2016-11-24 14:11:41 +0800

    country blade

--- resources/views/country.blade.php
+++ resources/views/country.blade.php
@@ -1,12 +1,12 @@
-@extends('layouts.index-layout', ['currentView' => 'index-view'])
+@extends('layouts.index-layout', ['currentView' => 'country-view'])
 @section('content') 
 	<section class="content">
-	<div class="country-cover" style="background-image: url('{{asset('images/canada-mountain.jpg')}}')">
+	<div class="country-cover" style="background-image: url('{{$country->cover_url}}')">
 		<div class="text-outer-wrapper">
 			<div class="text-centerize">
 				<div class="country-cover-name">
-					<img src="{{asset('images/site/flag-of-canada.png')}}" />
-					Канад
+					<img src="{{$country->flag_url}}" />
+					{{$country->name}}
 					<a href="#section-content">
 						<i class="fa fa-sort-down"></i>
 					</a>
@@ -14,94 +14,76 @@
 			</div>
 		</div>
 	</div>
-		<!-- country card end -->
 		<div class="cntr-fluid" id="section-content" style="margin-top: -95px;">	
 			<div class="row margin-vertical">
 				<div class="col-md-3 col-xs-12 pull-right">
 					<div class="feed-container mini">
 						<div class="feed-title">
-							Канад
+							{{$country->name}}
 						</div>
 						<div class="country-navigator">
 							<ul>
-								<li>
-									<a>Канап улсын тухай</a>
+								<li class="active">
+									<a @click="setType('about')">Улсын тухай</a>
 								</li>
 								<li>
-									<a>Боловсролын тухай</a>
+									<a @click="setType('education')">Боловсролын систем</a>
 								</li>
 								<li>
-									<a>Сургуулиуд</a>
+									<a @click="setType('schools')">Сургуулиуд</a>
 								</li>
 								<li>
-									<a>Визний мэдээлэл</a>
+									<a @click="setType('visa')">Визний мэдээлэл</a>
 								</li>
 								<li>
-									<a>Тэтгэлэг</a>
+									<a @click="setType('scholarship')">Тэтгэлэг</a>
 								</li>
 								<li>
-									<a>Фото зураг</a>
+									<a @click="setType('news')">Мэдээлэл</a>
 								</li>
 							</ul>
 						</div>
 					</div>
 				</div>
 
-				<div class="col-md-9 col-xs-12">
+				<div class="col-md-9 col-xs-12" v-show="type == 'about'">
 					<div class="feed-container mini">
 						<div class="feed-title">
-							Сургуулиуд
-							<div class="university-filter-list">
-								<ul>
-									<li class="active">
-										Бакалавр
-									</li>
-									<li>
-										Магистр
-									</li>
-									<li>
-										Хэлний бэлтгэл
-									</li>
-								</ul>
-							</div>
+							Улсын тухай
 						</div>
 						<div class="content-container small university" >
-							<div class="cover-cntr">
-								<div class="cover">
-									<div class="cover-img">
-										<img class="horizontal vertical" src="{{asset('images/cambridge.png')}}"/>
-									</div>
-								</div>
-							</div>
-							<div class="content">
-								<div class="content-title">
-									Massachusetts Institute of Technology
-								</div>
-								<div class="content-description">
-									Massachusetts Institute of Technology Massachusetts Institute of Technology Massachusetts Institute of Technology
-									Massachusetts Institute of Technology Massachusetts Institute of Technology Massachusetts Institute of Technology Massachusetts Institute of Technology Massachusetts Institute of Technology Massachusetts Institute of Technology Massachusetts Institute of Technology
-								</div>
-							</div>
-						</div>
-						<div class="content-container small university" >
-							<div class="cover-cntr">
-								<div class="cover">
-									<div class="cover-img">
-										<img class="horizontal vertical" src="{{asset('images/cambridge.png')}}"/>
-									</div>
-								</div>
-							</div>
-							<div class="content">
-								<div class="content-title">
-									Massachusetts Institute of Technology
-								</div>
-								<div class="content-description">
-									Massachusetts Institute of Technology Massachusetts Institute of Technology Massachusetts Institute of Technology
-									Massachusetts Institute of Technology Massachusetts Institute of Technology Massachusetts Institute of Technology Massachusetts Institute of Technology Massachusetts Institute of Technology Massachusetts Institute of Technology Massachusetts Institute of Technology
-								</div>
-							</div>
+							{!!$country->countryInformation[0]->content!!}
 						</div>
 					</div>
+				</div>
+
+				<div class="col-md-9 col-xs-12" v-show="type == 'visa'">
+					<div class="feed-container mini">
+						<div class="content-container small university" >
+							{!!$country->countryVisa[0]->content!!}
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-9 col-xs-12" v-show="type == 'education'">
+					<div class="feed-container mini">
+						<div class="content-container small university" >
+							{!!$country->countryEducation[0]->content!!}
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-md-9 col-xs-12" v-if="type == 'schools'">
+					<country-schools :country="{{$country->id}}">
+						
+					</country-schools>
+				</div>
+
+				<div class="col-md-9 col-xs-12" v-if="type == 'news'">
+					<country-news :country="{{$country->id}}">
+						
+					</country-news>
 				</div>
 			</div>
 		</div>
