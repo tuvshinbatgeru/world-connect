0d7d8af97f4493016e786e3c179b080b79cc7510
AkilLis <g.tuvshinbat@yahoo.com>
2016-11-24 14:12:53 +0800

    new country instance

---
+++ resources/assets/js/web/CountryView.vue
@@ -0,0 +1,32 @@
+<script>
+	import CountrySchools from '../components/CountrySchools.vue'
+	import CountryNews from '../components/CountryNews.vue'
+
+	export default {
+		props : {
+			title : {
+				default : ''
+			},
+
+			type : {
+
+			}
+		},
+
+		data () {
+			return {
+				menu : 'dashboard-view'
+			}
+		},
+
+		methods : {
+			setType : function (type) {
+				this.type = type
+			}
+		},
+
+		components : {
+			CountrySchools, CountryNews
+		}
+	}
+</script>
