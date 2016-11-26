5131f50b0bd889e28c9bb63184e764bfdcdc8341
AkilLis <g.tuvshinbat@yahoo.com>
2016-11-26 13:24:58 +0800

    NEw componenets

---
+++ resources/assets/js/components/AlbumSlider.vue
@@ -0,0 +1,103 @@
+<template>
+	<div id="container" class="margin-vertical">
+	</div>
+
+	<div id="blueimp-gallery" class="blueimp-gallery">
+	    <div class="slides"></div>
+	    <h3 class="title"></h3>
+	    <a class="prev">‹</a>
+	    <a class="next">›</a>
+	    <a class="close">×</a>
+	    <a class="play-pause"></a>
+	    <ol class="indicator"></ol>
+	</div>
+</template>
+<script>
+	export default {
+		props : {
+			countryId : {
+				default : 0
+			}
+		},
+
+		data () {
+			return {
+				albums : [],
+				blueimpInstance : {},
+				masonryContainer : {}
+			}
+		},
+
+		created : function () {
+			this.getAlbums()
+		},
+
+		ready : function () {
+			this.blueimpInstance = blueimp
+			this.masonryContainer = $('#container').masonry({
+			    itemSelector: '.item',
+			    columnWidth: 200,
+			    gutter: 2
+			});
+		},
+
+		methods : {
+			getAlbums : function () {
+            	this.$http.get(this.$env.get('APP_URI') + 'album?country=' + this.countryId).then(res => {
+            	  	this.albums = res.data.result
+            	  	this.calcMasonry()
+            	}).catch(err => {
+            	});
+            },
+
+            browseAlbum : function (album_id) {
+            	this.$http.get(this.$env.get('APP_URI') + 'album/' + album_id + '/photos').then(res => {
+            		var slideItems = []
+            		_.forEach(res.data.result, function (photo) {
+            			slideItems.push({
+            				title : photo.pivot.caption,
+            				href: photo.url,
+            				thumbnail : photo.url
+            			})
+            		});
+
+            	  	blueimp.Gallery(slideItems)
+            	}).catch(err => {
+            	  
+            	});
+            },	
+
+            getMasonryItems : function () {
+				var items = ''
+
+				_.forEach(this.albums, function (album) {
+				  		items += 
+				  		'<div @click="browseAlbum(' + album.id + ')" class="item">'
+				  			+ '<div class="album-title-wrapper"> <div class="album-title">' + album.title +'<br/> <span>' + album.photos_count + ' зураг</span></div> </div>'
+				  			+ '<img src="' + album.pinned_photo[0].url + '"/>' 
+				    	+ '</div>'	
+				})
+
+			  	return $( items )
+            },
+
+            calcMasonry : function () {
+            	var $items = this.getMasonryItems()
+                var msnry = this.masonryContainer.data('masonry');
+			    var itemSelector = msnry.options.itemSelector;
+			  	// hide by default
+			  	$items.hide();
+			    // append to container
+			    var element = this.masonryContainer.append( $items )
+
+			    this.$compile(element.get(0))
+
+			    $items.imagesLoaded().progress( function( imgLoad, image ) {
+			    var $item = $( image.img ).parents( itemSelector );
+			    $item.show();
+			    msnry.appended( $item );
+			  });
+            },
+		}
+	}
+</script>
---
+++ resources/assets/js/web/SiteHeader.vue
@@ -0,0 +1,124 @@
+<template>
+	<section class="header">
+		<div class="cntr-fluid">
+				<div class="wc-nav">
+					<ul>
+						<li class="active">
+							<a>
+								Нүүр хуудас
+							</a>
+						</li>
+						<li>
+							<a>
+								Бидний тухай
+							</a>
+						</li>
+						<li>
+							<a>
+								Холбоо барих
+							</a>
+						</li>
+						<li>
+							<div class="search-input">
+								<i class="fa fa-search"></i>
+								<input placeholder="Хайлт хийх ..." type="text" />
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="wc-nav-logo">
+			<img src="{{ asset('images/logo-wc-vertical.png') }}" height="100" />
+		</div>
+		<hr class="header-seperator"></hr>
+		<div style="width: 100%;position: relative;">
+			<div class="cntr-fluid">
+				<div class="wc-content-nav">
+					<ul>
+						<li @mouseover="mouseOver('country')" @mouseout="mouseOut()" class="active">
+							<a>Зуучлах улсууд</a>
+						</li>
+						<li @mouseover="mouseOver('schools')" @mouseout="mouseOut()">
+							<a>Сургуулиуд</a>
+						</li>
+						<li @mouseover="mouseOver('scholar')" @mouseout="mouseOut()">
+							<a href="/news?type=scolarship">Тэтгэлэгт хөтөлбөр</a>
+						</li>
+						<li @mouseover="mouseOver('visa')" @mouseout="mouseOut()">
+							<a>Визний мэдээлэл</a>
+						</li>
+						<li @mouseover="mouseOver('english')" @mouseout="mouseOut()">
+							<a>Англи хэлний сургалт</a>
+						</li>
+						<li>
+							<a href="/news?type=news">Мэдээлэл</a>
+						</li>
+					</ul>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+			
+			<div class="card-container" :class="hovered ? 'show' : ''" @mouseover="mouseOver('all')" @mouseout="mouseOut()">
+				<div class="cntr-fluid">
+					<div class="card country" v-for="country in countries" v-show="selectedMenu == 'country'">
+						<div class="cover">
+							<div class="cover-img">
+								<img class="horizontal" :src="country.cover_url"/>
+							</div>
+							<div class="title">
+								<a :href="'/country/' + country.id"> {{country.name}}</a>
+							</div>
+						</div>
+					</div>
+
+					<div class="card country" v-for="country in countries" v-show="selectedMenu == 'visa'">
+						<div class="cover">
+							<div class="cover-img">
+								<img class="horizontal" :src="country.cover_url"/>
+							</div>
+							<div class="title">
+								<a :href="'/country/' + country.id + '?type=visa'"> {{country.name}}</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+<script>
+	export default {
+		data () {
+			return {
+				countries : [],
+				hovered : false,
+				selectedMenu : {},
+			}
+		},	
+
+		created : function () {
+			this.getCountries()
+		},
+
+		methods : {
+			mouseOver : function (type) {
+				this.hovered = true
+				if(type != 'all')
+					this.selectedMenu = type
+			},
+
+			mouseOut : function () {
+				this.hovered = false
+			},
+
+			getCountries : function () {
+				this.$http.get(this.$env.get('APP_URI') + 'country/site').then(res => {
+				  	this.countries = res.data.result
+				}).catch(err => {
+
+				});
+			}
+		}
+	}
+</script>
