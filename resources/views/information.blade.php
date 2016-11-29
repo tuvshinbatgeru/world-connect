a42a0d3641fc4873d4abae0e9693fac2a2b70a2a
AkilLis <g.tuvshinbat@yahoo.com>
2016-11-29 15:49:44 +0800

    all information page

---
+++ resources/views/information.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.index-layout', ['currentView' => 'information-view'])
+@section('content') 
+	<section class="content" v-cloak>
+		<div class="feed-container mini">
+			<div class="feed-title">
+				Мэдээ, Мэдээлэлүүд
+			</div>
+
+			<div class="content-container" v-for="news in information">
+					<div class="cover-cntr">
+						<div class="cover">
+							<div class="cover-img">
+								<img class="horizontal" :src="news.cover_url"/>
+							</div>
+						</div>
+					</div>
+					<div class="content">
+						<div class="content-title">
+							<a :href="'/news/' + news.id">@{{news.title}}</a>
+							<label class="date">
+								@{{news.created_at | moment 'from'}}
+							</label>
+						</div>
+						<div class="content-description">
+							@{{news.info[0].description}}
+						</div>
+						<div class="content-info">
+							<div class="row">
+								<div class="col-xs-6">
+									<span class="share social-share facebook">
+										SHARE
+									</span>
+									<span class="tweet social-share twitter">
+										TWEET
+									</span>
+								</div>
+								<div class="col-xs-6 text-right">
+									<span class="view">
+										<i class="fa fa-eye"></i> @{{news.visit_count}}
+									</span>
+									<span class="news-type">
+										@{{news.type | newsFilter}}
+									</span>
+								</div>	
+							</div>
+						</div>
+					</div>
+				</div>
+		</div>
+		<paginate :paginate="3" 
+		          v-ref:paginate
+		          :total.sync="total"
+		          @page-changed="pageChanged"
+		            >
+		</paginate>
+	</section>
+@stop
