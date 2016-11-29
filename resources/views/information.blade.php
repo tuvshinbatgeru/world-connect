2ec2110c8695a9b1078f399ca2891e8bcbc253e2
AkilLis <g.tuvshinbat@yahoo.com>
2016-11-29 16:06:01 +0800

    social buttons removed

--- resources/views/information.blade.php
+++ resources/views/information.blade.php
@@ -26,15 +26,7 @@
 						</div>
 						<div class="content-info">
 							<div class="row">
-								<div class="col-xs-6">
-									<span class="share social-share facebook">
-										SHARE
-									</span>
-									<span class="tweet social-share twitter">
-										TWEET
-									</span>
-								</div>
-								<div class="col-xs-6 text-right">
+								<div class="col-xs-12 text-right">
 									<span class="view">
 										<i class="fa fa-eye"></i> @{{news.visit_count}}
 									</span>
