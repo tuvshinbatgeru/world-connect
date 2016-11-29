923af8fd8d129da06dc70d26043c92ec05c218cc
AkilLis <g.tuvshinbat@yahoo.com>
2016-11-29 22:45:02 +0800

    news layout

---
+++ resources/views/layouts/news-layout.blade.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html>
+<head>
+	<meta property="og:url"                content="" />
+	<meta property="og:type"               content="Мэдээ" />
+	<meta property="og:title"              content="{{$news->title}}" />
+	<meta property="og:description"        content="{{$news->info[0]->description}}"/>
+	<meta property="og:image"              content="{{$news->cover_url}}" />
+	<meta property="fb:app_id"              content="1549615252014368" />
+
+    @include('includes.resource')
+    @include('includes.index-header')
+</head>
+<body>
+	<site-header>
+		
+	</site-header>
+
+	@if (isset($type))
+        <component is="{{$currentView}}" 
+        		   type="{{$type}}"
+                   inline-template>
+    @else
+    	<component is="{{$currentView}}" 
+                   inline-template>
+    @endif
+  	   	@yield('content')
+   	</component>
+
+	<section class="footer">
+		<div class="cntr-fluid">
+			<div class="row">
+				<div class="col-md-3 col-sm-6">
+					<div class="footer-item-container">
+						<div class="wc-logo">
+							<img src="{{ asset('images/logo-wc.png') }}" height="50" />
+						</div>
+						<hr></hr>
+						<div class="social">
+							<a class="facebook">
+								<i class="fa fa-facebook"></i>
+							</a>
+							<a class="twitter">
+								<i class="fa fa-twitter"></i>
+							</a>
+							<a class="google-plus">
+								<i class="fa fa-google-plus"></i>
+							</a>
+							<a class="skype">
+								<i class="fa fa-skype"></i>
+							</a>
+						</div>
+
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<div class="footer-item-container">
+						<label>Бидний тухай</label>
+						<ul>
+							<li>
+								<a>Эрхэм зорилго</a>
+							</li>
+							<li>
+								<a>Компани</a>
+							</li>
+							<li>
+								<a>Үйлчилгээ</a>
+							</li>
+							<li>
+								<a>Хамтын ажиллагаа</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<div class="footer-item-container">
+						<label>Бид юу хийдэг вэ</label>
+						<ul>
+							<li>
+								<a>Их сургуульд зуучлах</a>
+							</li>
+							<li>
+								<a>Визний бүх төрөлд зуучлах</a>
+							</li>
+							<li>
+								<a>Элчингийн ярилцлаганд бэлдэх</a>
+							</li>
+							<li>
+								<a>Англи хэлний сургалт</a>
+							</li>
+							<li>
+								<a>Албан ёсны орчуулга</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<div class="footer-item-container">
+						<label>Холбоо барих</label>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="{{ asset('js/blueimp-helper.js') }}"></script>
+	<script src="{{ asset('js/blueimp-gallery.js') }}"></script>
+	<script src="{{ asset('js/blueimp-gallery-fullscreen.js') }}"></script>
+	<script src="{{ asset('js/blueimp-gallery-indicator.js') }}"></script>
+	<script src="{{ elixir('js/app.js') }}"></script>
+</body>
+</html>
