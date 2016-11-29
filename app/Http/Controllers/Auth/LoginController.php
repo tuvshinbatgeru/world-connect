8cfc62a916fab3c0da7656d17c76f8c341cbff47
AkilLis <g.tuvshinbat@yahoo.com>
2016-11-29 15:50:59 +0800

    Login controller added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,9 @@ class LoginController extends Controller
     {
         $this->middleware('guest', ['except' => 'logout']);
     }
+
+    public function login(Request $request)
+    {
+        dd($request->all());
+    }
 }
