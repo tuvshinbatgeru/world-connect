874a265ce2e5dbe7faed9b3493e825fb31d275af
AkilLis <g.tuvshinbat@yahoo.com>
2016-11-29 16:06:15 +0800

    social buttons removed

--- resources/assets/js/components/CountryScholarship.vue
+++ resources/assets/js/components/CountryScholarship.vue
@@ -24,15 +24,7 @@
 					</div>
 					<div class="content-info">
 						<div class="row">
-							<div class="col-xs-6">
-								<span class="share social-share facebook">
-									SHARE
-								</span>
-								<span class="tweet social-share twitter">
-									TWEET
-								</span>
-							</div>
-							<div class="col-xs-6 text-right">
+							<div class="col-xs-12 text-right">
 								<span class="view">
 									<i class="fa fa-eye"></i> {{ship.visit_count}}
 								</span>
