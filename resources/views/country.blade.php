6c5ee04ba498daf9e8830d152493fda01e4daae3
Saruul\Saruultugs <saruultugs.azgun@gmail.com>
2016-11-30 13:18:39 +0800

    news countyr page

--- resources/views/country.blade.php
+++ resources/views/country.blade.php
@@ -1,52 +1,50 @@
 @extends('layouts.index-layout', ['currentView' => 'country-view'])
 @section('content') 
 	<section class="content">
-	<div class="country-cover" style="background-image: url('{{$country->cover_url}}')">
-		<div class="text-outer-wrapper">
-			<div class="text-centerize">
-				<div class="country-cover-name">
-					<img src="{{$country->flag_url}}" />
-					{{$country->name}}
-					<a href="#section-content">
-						<i class="fa fa-sort-down"></i>
-					</a>
-				</div>
-			</div>
-		</div>
-	</div>
-		<div class="cntr-fluid" id="section-content" style="margin-top: -95px;">	
-			<div class="row margin-vertical">
-				<div class="col-md-3 col-xs-12 pull-right no-padding">
-					<div class="feed-container mini">
-						<div class="feed-title">
+	<div class="country-background">
+		<div class="cntr-fluid country-wrapper">
+			<div class="country-cover" style="background-image: url('{{$country->cover_url}}')">
+				<div class="text-outer-wrapper">
+					<div class="text-centerize">
+						<div class="country-cover-name">
 							{{$country->name}}
 						</div>
-						<div class="country-navigator">
-							<ul>
-								<li :class="type == 'about' ? 'active' : ''">
-									<a @click="setType('about')">Улсын тухай</a>
-								</li>
-								<li :class="type == 'education' ? 'active' : ''">
-									<a @click="setType('education')">Боловсролын систем</a>
-								</li>
-								<li :class="type == 'schools' ? 'active' : ''">
-									<a @click="setType('schools')">Сургуулиуд</a>
-								</li>
-								<li :class="type == 'visa' ? 'active' : ''">
-									<a @click="setType('visa')">Визний мэдээлэл</a>
-								</li>
-								<li :class="type == 'scholarship' ? 'active' : ''">
-									<a @click="setType('scholarship')">Тэтгэлэг</a>
-								</li>
-								<li :class="type == 'news' ? 'active' : ''">
-									<a @click="setType('news')">Мэдээлэл</a>
-								</li>
-							</ul>
+						<div class="countr-description">
+							Танд Канад улсын шилдэг 50 сургуулиудаас сонгон суралцах боломж олгож байна.
 						</div>
 					</div>
 				</div>
+			</div>
+		</div>
+		<div class="cntr-fluid">
+			<div class="country-navigator">
+				<ul>
+					<li :class="type == 'about' ? 'active' : ''">
+						<a @click="setType('about')"> <i class="fa fa-globe"></i> {{$country->name}} улсын тухай</a>
+					</li>
+					<li :class="type == 'education' ? 'active' : ''">
+						<a @click="setType('education')"> <i class="fa fa-graduation-cap"></i> Боловсролын систем</a>
+					</li>
+					<li :class="type == 'visa' ? 'active' : ''">
+						<a @click="setType('visa')"> <i class="fa fa-address-card-o"></i> Визний мэдээлэл</a>
+					</li>
+					<li :class="type == 'schools' ? 'active' : ''">
+						<a @click="setType('schools')"> <i class="fa fa-university"></i> Сургуулиуд</a>
+					</li>
+					<li :class="type == 'scholarship' ? 'active' : ''">
+						<a @click="setType('scholarship')"> <i class="fa fa-gift"></i> Тэтгэлэг</a>
+					</li>
+					<li :class="type == 'news' ? 'active' : ''">
+						<a @click="setType('news')"> <i class="fa fa-newspaper-o"></i> Мэдээлэл</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+		<div class="cntr-fluid" id="section-content">	
+			<div class="row margin-vertical">
 
-				<div class="col-md-9 col-xs-12 no-padding" v-show="type == 'about'">
+				<div class="col-md-12 no-padding" v-show="type == 'about'">
 					<div class="feed-container mini">
 						<div class="feed-title">
 							Улсын тухай
@@ -57,7 +55,7 @@
 					</div>
 				</div>
 
-				<div class="col-md-9 col-xs-12 no-padding" v-show="type == 'visa'">
+				<div class="col-md-12 col-xs-12 no-padding" v-show="type == 'visa'">
 					<div class="feed-container mini">
 						<div class="feed-title">
 							Визний мэдээлэл
@@ -68,7 +66,7 @@
 					</div>
 				</div>
 
-				<div class="col-md-9 col-xs-12 no-padding" v-show="type == 'education'">
+				<div class="col-md-12 col-xs-12 no-padding" v-show="type == 'education'">
 					<div class="feed-container mini">
 						<div class="feed-title">	
 							Боловсролын систем
@@ -80,13 +78,13 @@
 				</div>
 
 
-				<div class="col-md-9 col-xs-12 no-padding" v-if="type == 'schools'">
+				<div class="col-md-12 col-xs-12 no-padding" v-if="type == 'schools'">
 					<country-schools :country="{{$country->id}}">
 						
 					</country-schools>
 				</div>
 
-				<div class="col-md-9 col-xs-12 no-padding" v-if="type == 'news'">
+				<div class="col-md-12 col-xs-12 no-padding" v-if="type == 'news'">
 					<country-news :country="{{$country->id}}">
 						
 					</country-news>
@@ -96,7 +94,7 @@
 
 				</country-scholarship>
 				
-				<div class="col-md-9 col-xs-12 no-padding margin-vertical">
+				<div class="col-md-12 col-xs-12 no-padding margin-vertical">
 					<album-slider :country-id="{{$country->id}}">
 						
 					</album-slider>
