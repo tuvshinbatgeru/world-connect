256e53aa7c9a23c2167c0e932029a4f42de504f3
Saruul\Saruultugs <saruultugs.azgun@gmail.com>
2016-11-30 13:18:12 +0800

    news full page

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,25 +1,46 @@
 @extends('layouts.news-layout', ['currentView' => 'news-view'])
 @section('content') 
 	<section class="content">
-	<div class="country-cover" style="background-image: url('{{$news->cover_url}}')">
-	</div>
-	<div class="cntr-fluid" id="section-content" style="margin-top: -95px;">	
-		<div class="row margin-vertical">
-			<div class="col-md-12 col-xs-12">
-				<div class="feed-container mini">
-					<div class="feed-title">
-						{{$news->title}}
-					</div>
-					<div class="content-container small university no-hover" >
+	<div class="news-background">
+		<div class="news-cover-wrapper">
+			<div class="news-cover" style="background-image: url('{{$news->cover_url}}')">
+			</div>
+		</div>
+		<div class="cntr-fluid" id="section-content">	
+			<div class="row">
+				<div class="col-md-12 col-xs-12">
+					<div class="news-full-page">
+						<h2>
+							{{$news->title}}
+						</h2>
+						<div class="date">
+							<div class="row">
+								<div class="col-xs-6 text-left">
+									Нийтэлсэн огноо 2016  11 сарын 28
+								</div>
+								<div class="col-xs-6 text-right">
+									<span class="facebook-share"> <i class="fa fa-facebook"></i> Share</span>
+								</div>
+							</div>
+						</div>
+						<hr></hr>
 						{!!$news->info[0]->content!!}
+						<div class="news-navigator">
+							<div class="previous">
+								<a><i class="fa fa-angle-left"></i> ӨМНӨХ МЭДЭЭ</a>
+							</div>
+							<div class="next">
+								<a>ДАРААГИЙН МЭДЭЭ <i class="fa fa-angle-right"></i></a>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
 
-		<related-news :news-id="{{$news->id}}">
-			
-		</related-news>
+			<related-news :news-id="{{$news->id}}">
+				
+			</related-news>
+		</div>
 	</div>
 
 	<div id="fb-root">
