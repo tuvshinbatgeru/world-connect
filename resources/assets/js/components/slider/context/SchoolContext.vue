2d9b9cf6dc19b046c77cb69eb3c5578cd34d725c
AkilLis <g.tuvshinbat@yahoo.com>
2016-11-30 14:36:53 +0800

    SChoolContext added

---
+++ resources/assets/js/components/slider/context/SchoolContext.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="university">
+		<table>
+			<tr>
+				<td class="university-img">
+					<div class="cover-img">
+						<img class="fit" :src="item.logo_url"/>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td class="university-name table--cell">
+					{{item.name}}
+				</td>
+			</tr>
+		</table>
+
+	</div>
+</template>
+<script>
+
+	export default {
+		props : {
+			item: {
+				required : true
+			}
+		},
+	}
+</script>
