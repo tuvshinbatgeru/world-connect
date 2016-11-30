ad9affe23dd2586fb540739336e99c8630380e8b
Saruul\Saruultugs <saruultugs.azgun@gmail.com>
2016-11-30 13:23:54 +0800

    album slide

---
+++ resources/assets/js/components/slider/context/AlbumSlide.vue
@@ -0,0 +1,26 @@
+<template>
+	<div @click="browseAlbum(item.id)" class="album-slide-item">
+		<div class="album-title-wrapper"> 
+			<div class="album-title"> {{item.title}} <br/> <span> {{item.photos_count}} зураг</span>
+			</div> 
+		</div>
+		<div class="cover-img">
+			<img class="vertical" :src="item.pinned_photo[0].url"/> 
+		</div>
+		
+	</div>
+</template>
+<script>
+
+	export default {
+		props : {
+			item: {
+				required : true
+			}
+		},
+
+	}
+</script>
+<style lang="scss">
+
+</style>
